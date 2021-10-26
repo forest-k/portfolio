59e2f973141b8e0cbcaa21d93c3cf7b96c9ae44b
forest-k <60856399+forest-k@users.noreply.github.com>
2021-10-26 15:33:12 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -1,59 +1,39 @@
 "use strict";
 // -> 미친짓을 못하도록 미연에 방지하기 위한 명령
 
-// Make navbar transparent when it is on the top
-const navbar = document.querySelector("#navbar");
-const navbarHeight = navbar.getBoundingClientRect().height;
+window.onload = function () {
+  // Make navbar transparent when it is on the top
+  const navbar = document.querySelector("#navbar");
+  const navbarHeight = navbar.getBoundingClientRect().height;
 
-document.addEventListener("scroll", () => {
+  document.addEventListener("scroll", () => {
+    if (window.scrollY >= navbarHeight) {
+      navbar.classList.add("navbar--dark");
+    } else {
+      navbar.classList.remove("navbar--dark");
+    }
+  });
+  // 스크롤이 될 때마다 내가 등록한 이 함수를 호출해라
+  // () =>, 'Arrow function' : 아무런 인자를 받지 않고 원하는 블럭을 실행
+  // `` : 백틱
+
   // console.log(window.scrollY);
   // console.log(`navbarHeight: ${navbarHeight}`);
   // -> 'navbar'의 height 값 확인하기
 
-  if (window.scrollY >= navbarHeight) {
-    navbar.classList.add("navbar--dark");
-  } else {
-    navbar.classList.remove("navbar--dark");
-  }
-});
-// 스크롤이 될 때마다 내가 등록한 이 함수를 호출해라
-// () =>, 'Arrow function' : 아무런 인자를 받지 않고 원하는 블럭을 실행
-// `` : 백틱
+  // Handle scrolling when tapping on the navbar menu
+  const navbarMenu = document.querySelector(".navbar__menu");
+  navbarMenu.addEventListener("click", (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+      return;
+    }
 
-// Avatar motion
-var $wrap = $(".home__avatar"),
-  lFollowX = 5,
-  lFollowY = 10,
-  x = 0,
-  y = 0,
-  friction = 1 / 12;
+    console.log(event.target.dataset.link);
 
-function animate() {
-  x += (lFollowX - x) * friction;
-  y += (lFollowY - y) * friction;
-
-  $wrap.css({
-    transform:
-      "translate(-50%, -50%) perspective(600px) rotateY(" +
-      -x +
-      "deg) rotateX(" +
-      y +
-      "deg)",
+    // Javascript scroll to id
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
   });
-  window.requestAnimationFrame(animate);
-}
-
-$(window).on("mousemove click", function (e) {
-  var lMouseX = Math.max(
-    -100,
-    Math.min(100, $(window).width() / 2 - e.clientX)
-  );
-  var lMouseY = Math.max(
-    -100,
-    Math.min(100, $(window).height() / 2 - e.clientY)
-  );
-  lFollowX = (12 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-  lFollowY = (10 * lMouseY) / 100;
-});
-
-animate();
+};
