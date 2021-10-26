ade4a1ea6a05852fe4de1f9fb01e272bb58353a0
forest-k <60856399+forest-k@users.noreply.github.com>
2021-10-26 13:46:45 +0900

    JS: Navbar

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+"use strict";
+// -> 미친짓을 못하도록 미연에 방지하기 위한 명령
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  // console.log(window.scrollY);
+  // console.log(`navbarHeight: ${navbarHeight}`);
+  // -> 'navbar'의 height 값 확인하기
+
+  if (window.scrollY >= navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
+// 스크롤이 될 때마다 내가 등록한 이 함수를 호출해라
+// () =>, 'Arrow function' : 아무런 인자를 받지 않고 원하는 블럭을 실행
+// `` : 백틱
+
+// Avatar motion
 var $wrap = $(".home__avatar"),
   lFollowX = 5,
   lFollowY = 10,
@@ -34,4 +57,3 @@ $(window).on("mousemove click", function (e) {
 });
 
 animate();
-o;
